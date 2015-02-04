5356a85dc676d8552c058391e06066dd2947341e
harits wahyu <harits.wahyu.hid@gmail.com>
2015-02-04 14:16:04 +0700

    Update host.js

--- js/host.js
+++ js/host.js
@@ -1,4 +1,4 @@
-var hostnya="http://192.168.1.4/mobile/1.7_fixedcolserver2/newver/";
-//var hostnya="http://semeru.dnset.com/mobile/newver/";
+//var hostnya="http://192.168.1.4/mobile/1.7_fixedcolserver2/newver/";
+var hostnya="http://semeru.dnset.com/mobile/newver/";
 //var hostnya="http://localhost/kantor/";
 //
